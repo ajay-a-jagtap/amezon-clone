53ef3e593254fc7c3f1cedcde02f24e4479fe29a
ajay-a-jagtap <ajayjagtap6666@gmail.com>
2024-12-19 13:17:32 +0530

    Initial commit: Amazon Clone project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Smooth scroll for internal links
+$('a.nav-link').on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
+    }
+});
